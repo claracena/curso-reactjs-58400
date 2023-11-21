1e9a1b16dee8522134dd902135550fcd6a88747c
CESAR ARACENA <aracenameister@gmail.com>
2023-11-21 18:13:01 -0300

    3ra version

--- pc-shop/src/components/LoadingContextProvider.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState, useEffect, createContext } from 'react';
-export const LoadingContext = createContext();
-
-const LoadingContextProvider = (props) => {
-    const [loading, setLoading] = useState({ loading: true });
-    return <LoadingContext.Provider value={{ loading, setLoading }}>{props.children}</LoadingContext.Provider>;
-};
-
-export default LoadingContextProvider;
